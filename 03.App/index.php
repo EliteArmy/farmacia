818266dfffc8bed8182e3f2be6224d0eb65bb996
EliteArmy <ariel9501@yahoo.com>
2019-02-02 00:26:36 -0600

    Inicio de Pagina Principal
    Inicio de Pagina Principal

--- 03.App/index.php
+++ 03.App/index.php
@@ -3,16 +3,70 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
   <title>Facturación</title>
-  <!-- <link rel="icon" href="./img/icon.png"> -->
+  
+  <link rel="icon" href="./img/icon.png">
   <link rel="stylesheet" href="./css/bootstrap.min.css">
-  <link rel="stylesheet" href="./css/style.css">
+  <!--<link rel="stylesheet" href="./css/style.css">-->
 </head>
-<body class="root">
+<!-- <body class="root"> -->
+<body>
+  
+  <!-- Barra de Navegación -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Barra de Navegación</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Principal <span class="sr-only">(Actual)</span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Zelda</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Facturas
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Buscar Factura</a>
+            <a class="dropdown-item" href="#">Nueva Factura</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Placeholder</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Desactivado</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+    </div>
+  </nav>
+  <!-- FIN Barra de Navegación -->
+
+  <!-- Contenido -->
+  <div class="text-center">
+    <h2>Farmacia Esperanza</h2>
+    <h2>Col. Villa Olímpica, Tegucigalpa M.D.C, Honduras</h2>
+    <h2>RTN: 08011980123456</h2>
+  </div>
+  <!-- FIN Contenido -->
+
   <script src="./js/jquery.min.js"></script>
   <script src="./js/bootstrap.min.js"></script>
   <script src="./js/popper.min.js"></script>
-  <script>
-  </script>
+
+  <!-- Custom Javascript -->
+  <!--<script src="./js/placeholder.js"></script> -->
 </body>
 </html>
