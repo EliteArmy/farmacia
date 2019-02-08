5248930e645f98c034b0e6c9044c1fc60779c61c
EliteArmy <ariel9501@yahoo.com>
2019-02-08 16:48:41 -0600

    inicio de Login

---
+++ 03.App/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <title>Login</title>
+  
+  <link rel="icon" href="./img/icon.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/style2.css">
+</head>
+
+<body>
+  
+  <!-- Contenido -->
+  <div class="container-fluid">
+    <div class="row justify-content-center ">
+      <div class="col-10 col-md-6 contenedor">
+        
+        <div class="row justify-content-center">
+          <div class="col-10 col-sm-8 col-lg-8 texto">
+            <h4>
+              Inicia Sesion con tu usuario
+            </h4>
+            <form method="POST" action="">
+              
+            </form>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+  </div>
+  <!-- FIN Contenido -->
+
+  <script src="./js/jquery.min.js"></script>
+  <script src="./js/popper.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+
+  <!-- Custom Javascript -->
+  <!--<script src="./js/placeholder.js"></script> -->
+</body>
+</html>
