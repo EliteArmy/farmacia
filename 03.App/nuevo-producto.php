39ab4073eb819ac5e9be8465f7ba0816ff2abf85
EliteArmy <ariel9501@yahoo.com>
2019-02-10 16:56:01 -0600

    Modal Nuevo Producto

--- 03.App/nuevo-producto.php
+++ 03.App/nuevo-producto.php
@@ -32,6 +32,72 @@
   </div>
   <!-- FIN Contenido -->
 
+  <!-- Button trigger para el modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#agregarproducto">
+          Agregar Nuevo Producto
+        </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="agregarproducto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Agregar Nuevo Producto</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        
+        <div class="modal-body">
+          <div class="row">
+            <div class="form-group col-12 col-md-6">
+              <label for="nombre-producto">Nombre del Producto</label>
+              <input type="text" class="form-control" id="nombre-producto" name="nombre-producto"  placeholder="Nombre del Producto" required autofocus>
+            </div>
+
+            <div class="form-group col-12 col-md-6">
+              <label for="Codigo-barra">Código de Barra</label>
+              <input type="text" class="form-control" id="Codigo-barra" name="" placeholder="Código de Barra">
+            </div>
+
+            <div class="form-group col-12 col-md-6">
+              <label for=""></label>
+              <input type="text" class="form-control" id="" name="" placeholder="">
+            </div>
+
+            <div class="form-group col-12 col-md-6">
+              <label for=""></label>
+              <input type="text" class="form-control" id="" name="" placeholder="">
+            </div>
+
+            <div class="form-group col-12 col-md-6">
+              <div class="btn-group">
+                  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categorias
+                  </button>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Categoria1</a>
+                    <a class="dropdown-item" href="#">Categoria2</a>
+                    <a class="dropdown-item" href="#">Categoria3</a>
+                    <a class="dropdown-item" href="#">Categoria4</a>
+                  </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary">Guardar Cambios</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!-- FIN Modal -->
+
   <script src="./js/jquery.min.js"></script>
   <script src="./js/popper.min.js"></script>
   <script src="./js/bootstrap.min.js"></script>
