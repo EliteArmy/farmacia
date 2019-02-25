6e66b6f1a186281c9dc674140b9dbbcabfb08a6f
EliteArmy <ariel9501@yahoo.com>
2019-02-24 20:34:12 -0600

    correccion

--- 03.App/login.php
+++ 03.App/login.php
@@ -25,56 +25,57 @@
   <section class="login">
 
   </section>
+  
   <!-- Contenido -->
-<div class="jumbotron vertical-center">
-  <div class="container">
-    <div class="row justify-content-center align-middle">
-      <div class="col-10 col-sm-9 col-md-7 col-lg-5 col-xl-4 well card">
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <div class="row justify-content-center align-middle">
+        <div class="col-10 col-sm-9 col-md-7 col-lg-5 col-xl-4 well card">
 
-        <div class="row justify-content-center">
-          <div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 texto">
-            <div class="row justify-content-center">
-              <img src="./img/icon.png" alt="Logo" width="50px" height="50px">
-            </div>
-            <h4 class="text-center">Farmacia Esperanza</h4>
-            <hr>
-            <h4 class="text-center">Inicio de sesión</h4>
-            
-            <div class="form-group">
-              <label for="user">Usuario</label>
-              <input type="text" class="form-control" name="username" id="username" placeholder="Usuario" required autofocus>
-            </div>
-
-            <div class="form-group">
-              <label for="pass">Contraseña</label>
-              <input type="password" class="form-control" name="password" id="password"  placeholder="Contraseña" required>
-            </div>
-
-            <div class="alert alert-danger" style="display: none;" id="div-error-login" role="alert">
-              <!--<span class="fas fa-exclamation" aria-hidden="true"></span>-->
-              <span class="sr-only">Error:</span>
-              Introduzca un Usuario y Contraseña validos 
-            </div>
-
-            <div >
-              <div class="text-left" style="display:inline-block;">
-                <input class="text-left" type="checkbox" name="" onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"> Mostrar Contraseña
-                <!--<span>Ha olvidado su Contraseña?</span>-->
+          <div class="row justify-content-center">
+            <div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 texto">
+              <div class="row justify-content-center">
+                <img src="./img/icon.png" alt="Logo" width="50px" height="50px">
               </div>
-                           
-              <div class="text-right" style="float:right">
-                <button type="submit" id="btn-login" class="btn btn-primary ">Ingresar</button>
+              <h4 class="text-center">Farmacia Esperanza</h4>
+              <hr>
+              <h4 class="text-center">Inicio de sesión</h4>
+              
+              <div class="form-group">
+                <label for="user">Usuario</label>
+                <input type="text" class="form-control" name="username" id="username" placeholder="Usuario" required autofocus>
+              </div>
+
+              <div class="form-group">
+                <label for="pass">Contraseña</label>
+                <input type="password" class="form-control" name="password" id="password"  placeholder="Contraseña" required>
+              </div>
+
+              <div class="alert alert-danger" style="display: none;" id="div-error-login" role="alert">
+                <!--<span class="fas fa-exclamation" aria-hidden="true"></span>-->
+                <span class="sr-only">Error:</span>
+                Introduzca un Usuario y Contraseña validos 
+              </div>
+
+              <div >
+                <div class="text-left" style="display:inline-block;">
+                  <input class="text-left" type="checkbox" name="" onchange="document.getElementById('password').type = this.checked ? 'text' : 'password'"> Mostrar Contraseña
+                  <!--<span>Ha olvidado su Contraseña?</span>-->
+                </div>
+                            
+                <div class="text-right" style="float:right">
+                  <button type="submit" id="btn-login" class="btn btn-primary ">Ingresar</button>
+                </div>
+                
               </div>
               
             </div>
-            
           </div>
-        </div>
 
+        </div>
       </div>
     </div>
   </div>
-</div>
   <!-- FIN Contenido -->
 
   <footer>
