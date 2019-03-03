6c0fe8a49a2b9d1b2a312ace126fbf9e58a7d76a
WilianBarahona <wilianbarahona10m@gmail.com>
2019-03-02 22:15:26 -0600

    Estandarizacion Insertar impuesto

--- 01.SQL/02.SQL/03.PLSQL/05.productos/03.impuesto/01.SP_Insertar_Impuesto.sql
+++ 01.SQL/02.SQL/03.PLSQL/05.productos/03.impuesto/01.SP_Insertar_Impuesto.sql
@@ -8,50 +8,50 @@ InsertarImpuesto:BEGIN
 -- Declaraciones
   DECLARE mensaje VARCHAR(255);
   DECLARE contador INT;
-  DECLARE resultado BOOLEAN;
-  DECLARE ultimoID INT;
+  DECLARE error BOOLEAN;
+  # DECLARE ultimoID INT;
 
 -- Inicializaciones
   SET mensaje='';
-  SET resultado = FALSE;
+  SET error = TRUE;
   SET contador=0;
-  SET ultimoId = 0;
+  # SET ultimoId = 0;
 -- _________________VALIDACIONES________________________________
   IF par_impuesto = '' OR par_impuesto IS NULL THEN
-    SET mensaje = '[nombre del impuesto]';
+    SET mensaje = CONCAT(mensaje, 'Nombre de impuesto vacio, ');
+  ELSE
+    SELECT COUNT(*) INTO contador
+    FROM impuesto WHERE UPPER(descripcion) LIKE UPPER(par_impuesto);
+
+    IF contador>0 THEN
+      SET mensaje =CONCAT(mensaje, 'El impuesto ya existe, ');
+    END IF;
+
   END IF;
+
   IF par_valor IS NULL OR par_valor = 0 OR par_valor = '' THEN
-    SET mensaje = '[valor > 0]';
+    SET mensaje = CONCAT(mensaje, 'El impuesto debe ser mayor que cero, ');
   END IF;
 
   IF mensaje != '' THEN
-    SET mensaje = CONCAT('Se necesitan los siguientes campos: ', mensaje);
-    SELECT mensaje, resultado;
+    SELECT mensaje, error;
     LEAVE InsertarImpuesto;
   END IF;
 
-  SELECT COUNT(*) INTO contador
-  FROM impuesto WHERE UPPER(descripcion) LIKE UPPER(par_impuesto);
 
-  IF contador>0 THEN
-    SET mensaje = 'Nombre de impuesto duplicado';
-    SELECT mensaje, resultado;
-    LEAVE InsertarImpuesto;
-  end if;
 
   SET AUTOCOMMIT = 0;
   START TRANSACTION;
 
   INSERT INTO impuesto (descripcion, porcentaje, estado, fecha_inicio) VALUES
   (par_impuesto, par_valor, 'A', CURDATE());
-  SELECT LAST_INSERT_ID() INTO ultimoID;
+  # SELECT LAST_INSERT_ID() INTO ultimoID;
   COMMIT;
 
-  SET mensaje = 'inserción con éxito';
-  SET resultado = TRUE;
-  SELECT *, mensaje, resultado
-  FROM impuesto
-  WHERE id_impuesto = ultimoID;
+  SET mensaje = 'Inserción exitosa';
+  SET error = FALSE;
+  SELECT mensaje, error;
+
 END $$
 
 # DELIMITER ;
