83a63b1060c254d2b1e65f2ed93c0bc82b171fa9
Jose Antonio Aguilar <jaguilar992@gmail.com>
2019-03-13 18:57:49 -0600

    Update 06.VistaProducto.sql

--- 01.SQL/02.SQL/02.VISTAS/06.VistaProducto.sql
+++ 01.SQL/02.SQL/02.VISTAS/06.VistaProducto.sql
@@ -1,7 +1,6 @@
---CREATE OR REPLACE VIEW VistaProducto AS 
+CREATE OR REPLACE VIEW VistaProducto AS
 SELECT
   *
-  ,(pro.id_producto IN (SELECT id_producto FROM medicamentos)) as es_medicamento
   ,(SELECT presentacion FROM presentacion WHERE pro.id_presentacion = id_presentacion) as presentacion
   ,CASE WHEN (pro.id_producto IN (SELECT id_producto FROM medicamentos)) = TRUE
     THEN (SELECT nombre_laboratorio FROM laboratorio WHERE id_laboratorio = (SELECT id_laboratorio FROM medicamentos WHERE id_producto = pro.id_producto))
@@ -16,5 +15,9 @@ SELECT
     WHERE cat_prod.id_producto = pro.id_producto
     GROUP BY id_producto
   ) as categoria
-        
+  ,(CASE WHEN (SELECT id_producto FROM medicamentos WHERE id_producto = pro.id_producto) THEN 'M' ELSE 'P' END) as es_medicamento
 FROM producto pro;
+
+
+
+-- SELECT * FROM VistaProducto
