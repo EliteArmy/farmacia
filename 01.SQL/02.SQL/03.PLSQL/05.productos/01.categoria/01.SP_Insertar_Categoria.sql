677e9771e611c4b62f54b9f390eabfd63732d6ee
Androimeda <mnunezo@unah.hn>
2019-03-15 15:22:42 -0600

    PL_categoria modified

--- 01.SQL/02.SQL/03.PLSQL/05.productos/01.categoria/01.SP_Insertar_Categoria.sql
+++ 01.SQL/02.SQL/03.PLSQL/05.productos/01.categoria/01.SP_Insertar_Categoria.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 DROP PROCEDURE IF EXISTS SP_Insertar_Categoria $$
 CREATE PROCEDURE SP_Insertar_Categoria(
-  par_categoria VARCHAR(45)
+  pI_categoria VARCHAR(45)
 )
 InsertarCategoria:BEGIN
 -- Declaraciones
@@ -15,25 +15,25 @@ InsertarCategoria:BEGIN
   SET error = TRUE;
   SET contador=0;
   SET ultimoId = 0;
-
-  IF par_categoria = '' OR par_categoria IS NULL THEN
+-- _____________________VALIDACIONES_____________________________
+  IF pI_categoria = '' OR pI_categoria IS NULL THEN
     SET mensaje = 'Categoria vacia';
     SELECT mensaje, error;
     LEAVE InsertarCategoria;
   END IF;
 
   SELECT count(*) INTO contador FROM categoria
-  WHERE UPPER(categoria) LIKE UPPER(par_categoria);
+  WHERE UPPER(categoria) LIKE UPPER(pI_categoria);
   IF contador > 0 THEN
-    SET mensaje = CONCAT('La categoria ', par_categoria, ' ya existe');
+    SET mensaje = CONCAT('La categoria ', pI_categoria, ' ya existe');
     SELECT mensaje, error;
     LEAVE InsertarCategoria;
   END IF;
 
   SET AUTOCOMMIT = 0;
   START TRANSACTION;
-
-  INSERT INTO categoria (categoria) VALUES (par_categoria);
+-- ____________________CUERPO DEL PL_______________________________
+  INSERT INTO categoria (categoria, estado) VALUES (pI_categoria, "A");
   # SELECT LAST_INSERT_ID() INTO ultimoId;
   COMMIT;
 
