8a0b29c4b199b1cbd49dc7d95e7271a7d2b89b27
WilianBarahona <wilianbarahona10m@gmail.com>
2019-03-17 01:56:04 -0600

    Monstrar Estado Gestionar-Info-Producto y Gestionar Laboratorio

--- 03.App/js/controlador/gestionar-laboratorios.js
+++ 03.App/js/controlador/gestionar-laboratorios.js
@@ -27,9 +27,9 @@ $(document).ready(function() {
       },
       columns: [
         { data: "nombre_laboratorio", title:"Nombre"},
-        { data: "estado", title:"estado"},
         { data: "direccion", title:"direccion"},
         { data: "telefono_laboratorio", title:"telefono"},
+        { data: "estado", title:"Estado"},
         { data: null, title: "Opción",
         render: function ( data, type, row, meta ) {
           return '<button type="button" onclick="funcionBuscar('+ row.id_producto +')" class="btn btn-default btn-sm" data-toggle="modal" data-target="#agregar-producto"><span class="far fa-edit edit"></span></button>'+
