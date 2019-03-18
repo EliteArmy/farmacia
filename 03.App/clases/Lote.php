e6d8770c8d5e3f1156c4bf862aa26f4a3e432d44
Jose Antonio Aguilar <jaguilar992@gmail.com>
2019-03-17 20:19:25 -0600

    FIX
    Backend: No se esta devolviendo un array de los lotes por código de barra. (facturacion.php)

--- 03.App/clases/Lote.php
+++ 03.App/clases/Lote.php
@@ -126,17 +126,16 @@ class Lote extends Producto{
 		else return null;
 	}
 
-	
-	public function leerPorCodigoBarra($conexion){		
-           $sql = "			SELECT *	FROM VistaInventarioMercaderia			
-           WHERE codigo_barra = '%s'		
-   ";		
-  $valores = [$this->getCodigoBarra()];		
-  $rows = $conexion->query($sql, $valores);		
-  if (count($rows)) return $rows[0];		
-  else return null;	
+
+	public function leerPorCodigoBarra($conexion){
+           $sql = "			SELECT *	FROM VistaInventarioMercaderia
+           WHERE codigo_barra = '%s'
+   ";
+  $valores = [$this->getCodigoBarra()];
+  $rows = $conexion->query($sql, $valores);
+  return $rows;
 }
-	
+
 	public function leerDescuentoPorId($conexion){
 		$sql = "SELECT * FROM descuento WHERE id_descuento = %d";
 		$valores = [$this->getIdDescuento()];
