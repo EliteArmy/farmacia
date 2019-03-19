5fcf34479a75d84b8a650fe1caeeab9e458fba4c
Androimeda <mnunezo@unah.hn>
2019-03-19 01:53:17 -0600

    SP_ Eliminar detalle Factura UPDATE loteexistencia

--- 01.SQL/02.SQL/03.PLSQL/07.facturacion/SP_Eliminar_Detalle_Factura.sql
+++ 01.SQL/02.SQL/03.PLSQL/07.facturacion/SP_Eliminar_Detalle_Factura.sql
@@ -13,6 +13,8 @@ CREATE PROCEDURE SP_Eliminar_Detalle_Factura(
    DECLARE mensaje VARCHAR(1000);
    DECLARE error BOOLEAN;
    DECLARE contador INT;
+   DECLARE cant INT;
+   DECLARE lote INT;
 
    -- Inicializaciones
    SET AUTOCOMMIT=0;
@@ -20,10 +22,13 @@ CREATE PROCEDURE SP_Eliminar_Detalle_Factura(
    SET mensaje = '';
    SET contador=0;
    SET error=FALSE;
+   SET cant =0;
+   SET lote=0;
 
    -- _______________Validaciones__________________
 
    IF pI_id_temporal='' OR pI_id_temporal IS NULL THEN
+     
      SET mensaje=CONCAT(mensaje,"identificador vacio, ");
    END IF;
 
@@ -47,11 +52,19 @@ CREATE PROCEDURE SP_Eliminar_Detalle_Factura(
    END IF;
 
    -- _______________SQL Statements_______________
-   UPDATE detalle_factura_temp
+   SELECT cantidad INTO cant FROM detalle_factura_temp WHERE id_temporal= pI_id_temporal;
+   SELECT id_lote INTO lote FROM detalle_factura_temp WHERE id_temporal= pI_id_temporal;
+
+   UPDATE loteexistencia
       SET
-         estado= 'I'
-      WHERE
-         id_temporal= pI_id_temporal;
+         loteexistencia.existencia = (loteexistencia.existencia + cant)
+   WHERE
+         lote= id_lote;
+
+   DELETE 
+   FROM detalle_factura_temp
+   WHERE id_temporal= pI_id_temporal;
+   
    COMMIT;
 
    SET mensaje= 'Eliminación exitosa';
@@ -62,6 +75,7 @@ CREATE PROCEDURE SP_Eliminar_Detalle_Factura(
 
 END$$
 
-CALL SP_Eliminar_Detalle_Factura(17,@mesaje,@error);
+CALL SP_Eliminar_Detalle_Factura(32,@mesaje,@error);
 -- SELECT @mesaje, @error
 -- SELECT * FROM detalle_factura_temp
+SELECT * FROM loteexistencia where id_lote=1;
