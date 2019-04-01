34c054a48393e104f4fe3faed85c10d2294a6671
Androimeda <mnunezo@unah.hn>
2019-04-01 02:15:33 -0600

    SP Eliminar Tabla Cotizacion, FINALIZADO

--- 01.SQL/02.SQL/03.PLSQL/08.cotizacion/04.SP_Eliminar_Filas_Detalle_Cotizacion.sql
+++ 01.SQL/02.SQL/03.PLSQL/08.cotizacion/04.SP_Eliminar_Filas_Detalle_Cotizacion.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS SP_Eliminar_Filas_Detalle_Factura_Temp$$
-CREATE PROCEDURE SP_Eliminar_Filas_Detalle_Factura_Temp(
+DROP PROCEDURE IF EXISTS SP_ELSP_Eliminar_Filas_Detalle_Cotizacion_Temp$$
+CREATE PROCEDURE SP_ELSP_Eliminar_Filas_Detalle_Cotizacion_Temp(
 	pI_id_empleado INT,
     
     pO_mensaje VARCHAR(1000),
@@ -26,7 +26,7 @@ SP:BEGIN
 	IF pI_id_empleado='' OR pI_id_empleado IS NULL THEN 
 		SET mensaje='Codigo de empleado vacio';
 	ELSE
-		SELECT COUNT(*) INTO contador FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
+		SELECT COUNT(*) INTO contador FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
         IF contador=0 THEN
 			SET mensaje='Este empleado no tiene facturas';
 		END IF;
@@ -40,17 +40,17 @@ SP:BEGIN
     END IF;
     
     -- Tabla temporal donde se guardaran los ids a eliminar
-    CREATE TEMPORARY TABLE temp
-	SELECT id_temporal,id_empleado FROM detalle_factura_temp 
+    CREATE TEMPORARY TABLE temp_cotizacion
+	SELECT id_temporal,id_empleado FROM detalle_cotizacion_temp 
 	WHERE id_empleado=pI_id_empleado;
     
     -- Seleccionar el id_temporal maximo y minimo para eliminar por rangos
-    SELECT MAX(id_temporal),MIN(id_temporal) INTO idMax,idMin FROM temp;
+    SELECT MAX(id_temporal),MIN(id_temporal) INTO idMax,idMin FROM temp_cotizacion;
         
     -- Contador de cuantos registros seran eliminados, asignarse al liminte del delete
-    SELECT COUNT(*) INTO contador FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
+    SELECT COUNT(*) INTO contador FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
 
-    DELETE FROM detalle_factura_temp
+    DELETE FROM detalle_cotizacion_temp
     WHERE id_temporal BETWEEN idMin AND idMax
     ORDER BY id_temporal DESC
     LIMIT contador;
@@ -66,9 +66,9 @@ SP:BEGIN
     SET pO_error=error;
 END$$
 
-CALL SP_Eliminar_Filas_Detalle_Factura_Temp(81,@mensaje,@error);
+CALL SP_ELSP_Eliminar_Filas_Detalle_Cotizacion_Temp(81,@mensaje,@error);
 
-SELECT * FROM detalle_factura_temp WHERE id_empleado=81
+SELECT * FROM detalle_cotizacion_temp WHERE id_empleado=81
 
 CALL SP_Insertar_Detalle_Factura(81,1,1,@mesaje,@error);
 
@@ -77,7 +77,7 @@ CALL SP_Insertar_Detalle_Factura(81,1,1,@mesaje,@error);
 SELECT GROUP_CONCAT(DISTINCT id_temporal
 					ORDER BY id_temporal ASC
 					SEPARATOR ',') AS ids_temporales
-FROM detalle_factura_temp
+FROM detalle_cotizacion_temp
 WHERE id_empleado=81
 GROUP BY id_empleado */
 
