734a50e28387f00e1a24873d598dfb1146ec1ab6
Androimeda <mnunezo@unah.hn>
2019-04-01 02:33:35 -0600

    SP_Obtener_Detalle_Cotizacion

--- 01.SQL/02.SQL/03.PLSQL/08.cotizacion/05.SP_Obtener_Detalle_Cotizacion.sql
+++ 01.SQL/02.SQL/03.PLSQL/08.cotizacion/05.SP_Obtener_Detalle_Cotizacion.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS SP_Obtener_Detalle_Factura$$
-CREATE PROCEDURE SP_Obtener_Detalle_Factura(
+DROP PROCEDURE IF EXISTS SP_Obtener_Detalle_Cotizacion$$
+CREATE PROCEDURE SP_Obtener_Detalle_Cotizacion(
    pI_id_empleado INT(11),
 
    pO_mensaje VARCHAR(1000),
@@ -13,10 +13,10 @@ SP:BEGIN
     DECLARE mensaje VARCHAR(1000);
     DECLARE error BOOLEAN;
     DECLARE contador INT;
-    DECLARE totalFactura DOUBLE;
-    DECLARE subTotalFactura DOUBLE;
-    DECLARE totalImpuestoFactura DOUBLE;
-    DECLARE totalDescuentoFactura DOUBLE;
+    DECLARE totalCotizacion DOUBLE;
+    DECLARE subTotalCotizacion DOUBLE;
+    DECLARE totalImpuestoCotizacion DOUBLE;
+    DECLARE totalDescuentoCotizacion DOUBLE;
 
     -- Inicializaciones
     SET AUTOCOMMIT=0;
@@ -30,7 +30,7 @@ SP:BEGIN
     IF pI_id_empleado='' OR pI_id_empleado IS NULL THEN
       SET mensaje=CONCAT(mensaje,"Codigo de empleado vacio, ");
     ELSE
-      SELECT COUNT(*) INTO contador FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
+      SELECT COUNT(*) INTO contador FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
       IF contador=0 THEN
         SET mensaje=CONCAT(mensaje,"Usted no ha facturado ningun producto, ");
       END IF;
@@ -45,30 +45,30 @@ SP:BEGIN
       LEAVE SP;
     END IF;
 
-    SELECT SUM(total) INTO totalFactura FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
-    SELECT SUM(sub_total) INTO subTotalFactura FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
-    SELECT SUM(total_impuesto) INTO totalImpuestoFactura FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
-    SELECT SUM(total_descuento) INTO totalDescuentoFactura FROM detalle_factura_temp WHERE id_empleado=pI_id_empleado;
+    SELECT SUM(total) INTO totalCotizacion FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
+    SELECT SUM(sub_total) INTO subTotalCotizacion FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
+    SELECT SUM(total_impuesto) INTO totalImpuestoCotizacion FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
+    SELECT SUM(total_descuento) INTO totalDescuentoCotizacion FROM detalle_cotizacion_temp WHERE id_empleado=pI_id_empleado;
 
     
-    SET totalFactura=ROUND(totalFactura,2);
-    SET subTotalFactura=ROUND(subTotalFactura,2);
-    SET totalImpuestoFactura=ROUND(totalImpuestoFactura,2);
-    SET totalDescuentoFactura=ROUND(totalDescuentoFactura,2);
+    SET totalCotizacion=ROUND(totalCotizacion,2);
+    SET subTotalCotizacion=ROUND(subTotalCotizacion,2);
+    SET totalImpuestoCotizacion=ROUND(totalImpuestoCotizacion,2);
+    SET totalDescuentoCotizacion=ROUND(totalDescuentoCotizacion,2);
 
     SET mensaje= 'Obtención de factura exitosa';
     SET error=FALSE;
     SET pO_mensaje=mensaje;
     SET pO_error=error;
 
-    SELECT *,subTotalFactura,totalFactura,totalImpuestoFactura,totalDescuentoFactura,mensaje,error
-    FROM detalle_factura_temp
+    SELECT *,subTotalCotizacion,totalCotizacion,totalImpuestoCotizacion,totalDescuentoCotizacion,mensaje,error
+    FROM detalle_cotizacion_temp
     WHERE id_empleado=pI_id_empleado;
 
-    CALL SP_Eliminar_Filas_Detalle_Factura_Temp(pI_id_empleado,@mensajeEliminarFilas,@errorEliminarFilas);
-    IF @errorEliminarFilas THEN
-      SET mensaje=@mensajeEliminarFilas;
-      SET error=@errorEliminarFilas;
+    CALL SP_Eliminar_Filas_Detalle_Cotizacion_Temp(pI_id_empleado,@mensajeEliminarFilasCotizacion,@errorEliminarFilasCotizacion);
+    IF @errorEliminarFilasCotizacion THEN
+      SET mensaje=@mensajeEliminarFilasCotizacion;
+      SET error=@errorEliminarFilasCotizacion;
       SET pO_mensaje=mensaje;
       SET pO_error=error;
       SELECT mensaje,error;
@@ -77,10 +77,10 @@ SP:BEGIN
 
 END$$
 
-CALL SP_Obtener_Detalle_Factura(81,@mesaje,@error);
+CALL SP_Obtener_Detalle_Cotizacion(81,@mesaje,@error);
 
 -- Consultas de prueba
-SELECT * FROM detalle_factura_temp WHERE id_lote=1;
-SELECT SUM(cantidad) as cantidad, id_lote FROM detalle_factura_temp WHERE id_lote=1;
+SELECT * FROM detalle_cotizacion_temp WHERE id_lote=1;
+SELECT SUM(cantidad) as cantidad, id_lote FROM detalle_cotizacion_temp WHERE id_lote=1;
 
 SELECT * FROM lote WHERE id_lote=1;
