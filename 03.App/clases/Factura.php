806bbc2f8266d6a96a2ddce554d4aae85f878b92
EliteArmy <ariel9501@yahoo.com>
2019-04-02 00:51:36 -0600

    pequeño error

--- 03.App/clases/Factura.php
+++ 03.App/clases/Factura.php
@@ -324,16 +324,16 @@ class Factura {
     		$pdf->Cell(47, 8, " Lps. ".number_format($rows[0]['subTotalFactura'], 2, '.', ','), 0, 1, 'R', false);
 
 		    $pdf->SetX(118);
-        $pdf->Cell(28, 8, "Impuestos:", 0, 0, 'R'); //cell(width, height, txt, border, ln, align)
+        $pdf->Cell(28, 8, "Descuento:", 0, 0, 'R'); //cell(width, height, txt, border, ln, align)
         
         $pdf->SetX(146);
-    		$pdf->Cell(47, 8, " Lps. ".number_format($rows[0]['subTotalFactura'], 2, '.', ','), 0, 1, 'R', false);
+    		$pdf->Cell(47, 8, " Lps. ".number_format($rows[0]['totalDescuentoFactura'], 2, '.', ','), 0, 1, 'R', false);
 
 		    $pdf->SetX(118);
-		    $pdf->Cell(28, 8, "Descuento:", 0, 0, 'R'); //cell(width, height, txt, border, ln, align)
+		    $pdf->Cell(28, 8, "Impuestos:", 0, 0, 'R'); //cell(width, height, txt, border, ln, align)
 
 		    $pdf->SetX(146);
-		    $pdf->Cell(47, 8, " Lps. ".number_format($rows[0]['totalDescuentoFactura'], 2 ,'.', ','), 0, 1, 'R', false);
+		    $pdf->Cell(47, 8, " Lps. ".number_format($rows[0]['totalImpuestoFactura'], 2 ,'.', ','), 0, 1, 'R', false);
 
         $pdf->SetFont('Arial', 'B', 12); // Muestra el Total en negritas
 
