d05f5742feee18fafc8597bb85dcd35a89c7e492
Jose Antonio Aguilar <jaguilar992@gmail.com>
2019-04-20 20:30:57 -0600

    Update cerrar_session.php

--- 03.App/cerrar_session.php
+++ 03.App/cerrar_session.php
@@ -3,7 +3,7 @@
   session_start();
   $id_empleado = $_SESSION['id_empleado'];
   $conexion = new Conexion();
-  $sql = "CALL SP_CerrarSesion(1);";
+  $sql = "CALL SP_CerrarSesion(%s);";
   $valores = [$id_empleado];
   $conexion->query($sql, $valores);
   $conexion->cerrar();
