8501ffc69ceaa21cd68032ba64fd034defa83419
Hector504 <bardaleshector41@gmail.com>
2019-04-21 23:06:15 -0500

    Update cotizacion.php

--- 03.App/services/cotizacion.php
+++ 03.App/services/cotizacion.php
@@ -44,10 +44,9 @@ if(isset($_POST['accion'])){
         $con1 = new Conexion();
         $con2 = new Conexion();
         $idEmpleado = ValidarPost::unsigned('id_empleado');
-      $email = ValidarPost::varchar('email');
+        $email = ValidarPost::varchar('email');
         $nombre = ValidarPost::varchar('nombre_cliente');
         $cot = new Cotizacion();
-        $correo = new Mail();
   
         $cot->setIdEmpleado($idEmpleado);
         $cot->setIdCliente('');
@@ -62,51 +61,21 @@ if(isset($_POST['accion'])){
           $fechaHora = $res['data'][0]['fechaHora'];
           $res['pdf'] = $cot->imprimirPDF($con2, $idCotizacion, $nombreEmpleado, $nombreCliente, $fechaHora);
         }
+        echo json_encode($res);
+        $correo = new Mail();
+
        $correo->setDireccion($email);
        $correo->setAsunto('Farmacia Esperanza :: Cotizaciones');
         $correo->setCuerpo('Saludos Estimado(a), Adjuntamos Cotizaciones.');
         $correo->setAdjunto('../'.$res['pdf']);
         $correo->enviar();
-        
-        
-       
+    
         $con1->cerrar();
         $con2->cerrar();
         $con1 = null;
         $con2 = null;
   
-      //}
-    
-      
-      /*else
-      {
-      $con1 = new Conexion();
-      $con2 = new Conexion();
-      $idEmpleado = ValidarPost::unsigned('id_empleado');
-      $cot = new Cotizacion();
-
-      $cot->setIdEmpleado($idEmpleado);
-      $cot->setIdCliente('');
-      $cot->setIdFarmacia('');
-
-      $res['data'] = $cot->insertarCotizacion($con1);
-
-      if($res['data'][0]['error']==0){
-        $idCotizacion = $res['data'][0]['idCotizacion'];
-        $nombreEmpleado = $res['data'][0]['nombreEmpleado'];
-        $nombreCliente = $res['data'][0]['nombreCliente'];
-        $fechaHora = $res['data'][0]['fechaHora'];
-        $res['pdf'] = $cot->imprimirPDF($con2, $idCotizacion, $nombreEmpleado, $nombreCliente, $fechaHora);
-      }
-     
-      $con1->cerrar();
-      $con2->cerrar();
-      $con1 = null;
-      $con2 = null;
-
-      
-      } */
-        echo json_encode($res);
+        
     break;
 
     case 'cancelar-cotizacion':
