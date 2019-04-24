1d119593818b5ea8360d5329809bb9a10f39e9d1
Androimeda <mnunezo@unah.hn>
2019-04-24 16:38:05 -0600

    404 controlador

---
+++ 03.App/js/controlador/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>404 Page - Not Found</title>
+      <script scr=".../js/jquery.min.js"></script>
+      <link rel="stylesheet" href=".../plugin/404-not-found/css/style.css">
+</head>
+<body>
+    <body class="bg-purple">
+        <div class="stars">
+            <div class="custom-navbar">
+                <div class="brand-logo">
+                <!-- <img src="../img/icon.png" width="80px"> -->
+                </div>
+            </div>
+            <div class="central-body">
+                <img class="image-404" src=".../img/text227.png" width="300px">
+                <a href="../index.php" class="btn-go-home" target="_blank">GO BACK HOME</a>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src=".../img/cohetito.svg" width="40px">
+                <div class="earth-moon">
+                    <img class="object_earth" src=".../img/earth.svg" width="100px">
+                    <img class="object_moon" src=".../img/lunita.svg" width="80px">
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src=".../img/armstrong.svg" width="140px">
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+        </div>
+    </body>
+    <script  src=".../plugin/404-not-found/js/index.js"></script>
+    
+</body>
+</html>
